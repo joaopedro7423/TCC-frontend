2e31bec22e16509a5160a8bb6da1828ec2bc42cd
Joao Pedro <joaopedro7423@hotmail.com>
2021-09-03 16:49:44 -0300

    feat: add auth token in api connection

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,17 @@
 import axios from "axios";
+import { parseCookies } from "nookies";
 
-export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_TCC,
-});
+export function setupAPIClient(ctx = undefined) {
+  let cookies = parseCookies(ctx);
+
+  const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_TCC,
+    headers: {
+      token: `Bearer ${cookies["TccToken"]}`,
+    },
+  });
+
+  return api;
+}
+
+export const api = setupAPIClient();
