1f94fe8c98f5a0c46eaae449ad4d8cbb335d5b4f
Joao Pedro <joaopedro7423@hotmail.com>
2021-09-13 16:24:19 -0300

    feat: create atividade component

---
+++ src/components/pages/professor/atividades/index.tsx
@@ -0,0 +1,24 @@
+import { Heading, Text } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/react";
+import { CadastroAtividade } from "./CadastroAtividade";
+import { ListarAtividades } from "./ListarAtividades";
+
+type ProjectProps = {
+  id_project: string;
+};
+
+export function AtividadesPage({ id_project }: ProjectProps) {
+  return (
+    <>
+      <Heading size="lg"> Cadastro de Atividade:</Heading>
+      <Flex p={3} mb={6}>
+        <CadastroAtividade id_project={id_project} />
+      </Flex>
+
+      <Heading size="lg"> Listagem de Atividades:</Heading>
+      <Flex p={3}>
+        <ListarAtividades id_project={id_project} />
+      </Flex>
+    </>
+  );
+}
