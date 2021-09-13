82de744e90bf2f2938bf749dd8669a15691cd603
Joao Pedro <joaopedro7423@hotmail.com>
2021-09-13 16:24:37 -0300

    feat: create list projects component

---
+++ src/components/pages/professor/projetos/ListarProjetos.tsx
@@ -0,0 +1,74 @@
+import {
+  Flex,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  Center,
+  Button,
+  Icon,
+} from "@chakra-ui/react";
+
+import { IoMdLink } from "react-icons/io";
+import { api } from "services/api";
+import { useEffect, useState } from "react";
+import useAuth from "hooks/auth";
+import NextLink from "next/link";
+
+type ProjetosResponse = {
+  id: string;
+  title: string;
+  description: string;
+};
+
+export function ListarProjetos() {
+  const [notificacao, setPropos] = useState<ProjetosResponse[]>([]);
+  const { token, user } = useAuth();
+
+  useEffect(() => {
+    async function GetPropos() {
+      const response = await api.get<ProjetosResponse[]>("/projects", {
+        headers: {
+          authorization: `Bearear ${token}`,
+        },
+      });
+
+      setPropos(response.data);
+      console.log(response);
+    }
+    GetPropos();
+  }, []);
+
+  return (
+    <>
+      <Table w="100%" variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Título:</Th>
+            <Th>Acessar Projeto:</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {notificacao.map((item, index) => (
+            <Tr key={index}>
+              <Td>{item.title}</Td>
+              <Td>
+                <Flex>
+                  <Center>
+                    <NextLink href={`/professor/projetos/${item.id}`}> 
+                    <Button  m={1} colorScheme="blue">
+                      <Icon as={IoMdLink} />
+                    </Button>
+                    </NextLink>
+                  </Center>
+                </Flex>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </>
+  );
+}
