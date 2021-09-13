b9407c340081cc9263af27fb99c3eea3a6c31709
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2021-09-12 21:16:57 -0400

    feat: new apresentation proposal and accept model page

--- src/components/pages/professor/propostas/alunos/AprovarProposta.tsx
+++ src/components/pages/professor/propostas/alunos/AprovarProposta.tsx
@@ -11,70 +11,48 @@ import {
   ModalFooter,
   useDisclosure,
   useToast,
-  Center
+  Center,
+  Text,
+  Stack,
 } from "@chakra-ui/react";
-import { BsPencil } from "react-icons/bs";
-import { IoMdEye } from 'react-icons/io'
+import { IoMdEye } from "react-icons/io";
 import { api } from "services/api";
-import * as yup from "yup";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
 import useAuth from "hooks/auth";
 import { useState } from "react";
-import { Input } from "components/shared/Input";
 
 interface IdPropostaProps {
   id: string;
   title: string;
   description: string;
+  userCreate: any;
 }
-const updateFormSchema = yup.object().shape({
-  title: yup.string().required("Titúlo obrigatória"),
-  description: yup.string().required("Descrição obrigatória"),
-});
 
 export default function AprovarProposta(proposta: IdPropostaProps) {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const toast = useToast();
-  const { token, user } = useAuth(); //esssa disgraça vira um hook kkkk
-
+  const { token, user } = useAuth();
   const [loading, setLoading] = useState(false);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(updateFormSchema),
-  });
 
-  type UpdateFormData = {
-    title: string;
-    description: string;
-  };
-
-  const handleUpdateProposta: SubmitHandler<UpdateFormData> = async (
-    values
-  ) => {
-    //console.log(values);
+  const handleUpdateProposta = async () => {
+    // console.log(token);
 
     try {
       setLoading(true);
-
-      const reponse = await api.put(
+      //  console.log(proposta.id);
+      const reponse = await api.patch(
         `/proposals/${proposta.id}`,
-        {
-          title: values.title,
-          description: values.description,
-        },
+        { null: null },
         {
           headers: {
             authorization: `Bearear ${token}`,
           },
         }
       );
+
+      //    console.log(reponse.data);
       toast({
         title: "Sucesso !!!",
-        description: "Sucesso ao atualizar a propostacação",
+        description: "Sucesso ao aceitar a proposta.",
         status: "success",
         duration: 2000,
         isClosable: true,
@@ -86,7 +64,7 @@ export default function AprovarProposta(proposta: IdPropostaProps) {
       //console.log(error.response.data);
       if (error.response) {
         toast({
-          title: "Erro ao atualizar a propostacação.",
+          title: "Erro ao aceitar a proposta.",
           description: error.response.data.message,
           status: "error",
           duration: 2000,
@@ -95,7 +73,7 @@ export default function AprovarProposta(proposta: IdPropostaProps) {
         });
       } else {
         toast({
-          title: "Erro ao atualizar a propostacação.",
+          title: "Erro ao aceitar a proposta.",
           description: "Alguma coisa aconteceu.",
           status: "error",
           duration: 2000,
@@ -109,43 +87,31 @@ export default function AprovarProposta(proposta: IdPropostaProps) {
   };
   return (
     <>
-    <Center>
-      <Button onClick={onOpen} m={1} colorScheme="teal">
-        <Icon as={IoMdEye} />
-      </Button>
+      <Center>
+        <Button onClick={onOpen} m={1} colorScheme="teal">
+          <Icon as={IoMdEye} />
+        </Button>
       </Center>
       <Modal isOpen={isOpen} onClose={onClose} size="2xl">
         <ModalOverlay />
-        <ModalContent as="form" onSubmit={handleSubmit(handleUpdateProposta)}>
-          <ModalHeader>{proposta.title}</ModalHeader>
+        <ModalContent>
+          <ModalHeader>Proposta</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            <Input
-              w="100%"
-              defaultValue={proposta.title}
-              placeholder="Titulo:"
-              type="name"
-              {...register("title")}
-              error={errors.title}
-            ></Input>
-
-            <Textarea
-            mt={5}
-              h={12}
-              size="lg"
-              borderColor="black"
-              variant="filled"
-              placeholder="Descrição:"
-              type="description"
-              {...register("description")}
-              error={errors.description}
-            >
-              {proposta.description}
-            </Textarea>
+            <Stack spacing={4}>
+              <Text> Criador: {proposta.userCreate.name} </Text>
+              <Text> Titulo :{proposta.title} </Text>
+              <Text> Descrição: {proposta.description}</Text>
+            </Stack>
           </ModalBody>
           <ModalFooter>
-            <Button m={2} type="submit" colorScheme="green" isLoading={loading}>
-              Atualizar
+            <Button
+              m={2}
+              onClick={handleUpdateProposta}
+              colorScheme="green"
+              isLoading={loading}
+            >
+              Aceitar
             </Button>
             <Button onClick={onClose}>Cancel</Button>
           </ModalFooter>
