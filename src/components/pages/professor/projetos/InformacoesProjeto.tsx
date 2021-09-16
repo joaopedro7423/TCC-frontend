91fb54843e3660116ebd2d191cc15a408c174841
Joao Pedro <joaopedro7423@hotmail.com>
2021-09-16 09:56:22 -0300

    feat: create info project component

---
+++ src/components/pages/professor/projetos/InformacoesProjeto.tsx
@@ -0,0 +1,53 @@
+import { Stack, Text, Flex } from "@chakra-ui/react";
+
+import { api } from "services/api";
+import { useEffect, useState } from "react";
+import useAuth from "hooks/auth";
+import DeletProjeto from "./DeletProjeto";
+
+type ProjectProps = {
+  id_project: string;
+};
+
+interface ProjetoResponse  {
+  id: string;
+  title: string;
+  description: string;
+
+};
+
+export function InformacoesProjeto({ id_project }: ProjectProps) {
+  const [projeto, setProjeto] = useState<ProjetoResponse>();
+  const { token, user } = useAuth();
+
+  useEffect(() => {
+    async function GetProjeto() {
+      //   console.log(id_project)
+      const response = await api.get<ProjetoResponse>(
+        `/projects/${id_project}`,
+        {
+          headers: {
+            authorization: `Bearear ${token}`,
+          },
+        }
+      );
+
+      setProjeto(response.data);
+      ////console.log(response);
+    }
+    GetProjeto();
+  }, []);
+
+  return (
+    <>
+     
+        <Stack direction={["column"]} spacing="1rem" align="stretch">
+          <Text>Título: {projeto?.title}</Text>
+          <Text>Descrição: {projeto?.description}</Text>
+            
+ 
+        <DeletProjeto id={id_project} />
+        </Stack>
+    </>
+  );
+}
