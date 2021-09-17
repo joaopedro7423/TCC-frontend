0be6978e67e730693b6054c97dcab78f5215faec
Joao Pedro <joaopedro7423@hotmail.com>
2021-09-17 16:07:23 -0300

    refactor: remove desnecessery components in index student

--- src/pages/student/index.tsx
+++ src/pages/student/index.tsx
@@ -1,66 +1,15 @@
-import { Box, Flex, Heading, useToast } from "@chakra-ui/react";
-
-import { useForm } from "react-hook-form";
-
-import { useContext, useEffect, useState } from "react";
-
-import { useRouter } from "next/router";
+import SidebarWithHeader from "components/shared/header";
 import { AuthContext } from "context/auth";
 import { withSSRAuthenticated } from "utils/auth/renderAuth";
-import SidebarWithHeader from "components/shared/header";
+
+import { Box, Flex, Heading } from "@chakra-ui/react";
+
+import { useContext } from "react";
+
 
 const Professor = () => {
-  const toast = useToast();
-
-  const router = useRouter();
-
-  const { user, token, signOut } = useContext(AuthContext); //esssa disgraça vira um hook kkkk
-
-  const [loading, setLoading] = useState(false);
-
-  const { handleSubmit } = useForm();
-
-  //verificando pelo client
- /* useEffect(() => {
-    if (!user || user.role != "professor") {
-      router.push("/");
-    }
-  }, []);
-*/
-  const handleLogout = async () => {
-    //console.log(values);
-
-    try {
-      setLoading(true);
-
-      await signOut();
-
-      toast({
-        title: "Sucesso",
-        description: "Você agora está Deslogado.",
-        status: "success",
-        duration: 4000,
-        isClosable: true,
-        position: "top-right",
-      });
-
-      router.push("/");
-    } catch (error) {
-      toast({
-        title: "Erro ao Deslogar a conta.",
-        description: "Alguma coisa aconteceu.",
-        status: "error",
-        duration: 4000,
-        isClosable: true,
-        position: "top-right",
-      });
-    } finally {
-      setLoading(false);
-    }
-    [];
-  };
-
-  //console.log(user.role)
+ 
+  const { user } = useContext(AuthContext); //esssa disgraça vira um hook kkkk
 
   return (
     <>
@@ -97,24 +46,3 @@ export const getServerSideProps = withSSRAuthenticated(async (ctx) => {
     props: {},
   };
 },{roles:["student"]});
-
-/*
-function Component() {
-
-    const userCanSeeButton = useCan("student")
-
-    return (
-        <>
-
-        <Can permission="adm">
-            <Text>Só adm pode ver</Text>
-            <Button>Adm</Button>
-        </Can>
-
-      {userCanSeeButton &&  <Button>Enviar Atividade</Button>}
-      </>
-    )
-}
-issoé do repositorio do git do lucas uma função do components que da a permissão de acesso a sua escolha
-e fazer o hook do can
-*/
