aac274e02973af8bd62df2929cd8a967e8d2e370
Joao Pedro <joaopedro7423@hotmail.com>
2021-09-20 16:52:49 -0300

    feat: create activities list by student component

---
+++ src/components/pages/student/atividades/ListarAtividades.tsx
@@ -0,0 +1,59 @@
+import { SimpleGrid } from "@chakra-ui/react";
+
+import { api } from "services/api";
+import { useEffect, useState } from "react";
+import useAuth from "hooks/auth";
+import { CardAtividade } from "./CardAtividade";
+
+type ProjectProps = {
+  id_project: string;
+};
+
+type AtividadesResponse = {
+  id: string;
+  title: string;
+  description: string;
+  status: string;
+};
+
+export function ListarAtividades({ id_project }: ProjectProps) {
+  const [atividade, setAtivi] = useState<AtividadesResponse[]>([]);
+  const { token, user } = useAuth();
+
+  useEffect(() => {
+    async function GetAtividade() {
+      // console.log(id_project)
+      const response = await api.get<AtividadesResponse[]>(
+        `/activities/listbyproject/${id_project}`,
+        {
+          headers: {
+            authorization: `Bearear ${token}`,
+          },
+        }
+      );
+
+      setAtivi(response.data);
+      ////console.log(response);
+    }
+    GetAtividade();
+  }, []);
+
+  return (
+    <>
+      <SimpleGrid columns={[1, 2, 1, 2]}>
+        {atividade.map(function (data) {
+          const { id, title, description, status } = data;
+          return (
+            <CardAtividade
+              key={id}
+              id={id}
+              title={title}
+              description={description}
+              status={status}
+            />
+          );
+        })}
+      </SimpleGrid>
+    </>
+  );
+}
