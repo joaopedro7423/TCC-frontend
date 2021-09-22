2137ec13ce97520f68a50a718d19a8b41cbf7f77
Joao Pedro <joaopedro7423@hotmail.com>
2021-09-22 16:59:35 -0300

    feat: create notification card

---
+++ src/components/shared/header/NotificationsCard.tsx
@@ -0,0 +1,15 @@
+import { Text, Box } from "@chakra-ui/react";
+
+
+export default function NotificationsCard() {
+  return (
+    <>
+      <Box w="96%" borderRadius="lg" p={4} bgColor="white">
+        <Text fontWeight="bold" textTransform="uppercase" fontSize="lg">
+          Titulo
+        </Text>
+        <Text>Description jkasd sajkdn kasdm</Text>
+      </Box>
+    </>
+  );
+}
