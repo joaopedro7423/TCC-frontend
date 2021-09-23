0ce897cf0ecd9ddce9e6622a09d80b4a25e088dc
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2021-09-22 21:16:42 -0400

    feat: create list proposals professor component in student page proposals

---
+++ src/components/pages/student/professor/ListarPropostaProfessor.tsx
@@ -0,0 +1,65 @@
+import { Flex, Table, Thead, Tr, Th, Tbody, Td } from "@chakra-ui/react";
+
+import { api } from "services/api";
+import { useEffect, useState } from "react";
+import useAuth from "hooks/auth";
+import AprovarProposta from "./AprovarProposta";
+
+type PropostaResponse = {
+  id: string;
+  title: string;
+  description: string;
+  userCreate: any;
+};
+
+export function ListarPropostaProfessor() {
+  const [notificacao, setPropos] = useState<PropostaResponse[]>([]);
+  const { token, user } = useAuth();
+
+  useEffect(() => {
+    async function GetPropos() {
+      const response = await api.get<PropostaResponse[]>(
+        "/proposals/listByRoleAndCourse",
+        {
+          headers: {
+            authorization: `Bearear ${token}`,
+          },
+        }
+      );
+
+      setPropos(response.data);
+      // console.log(response);
+    }
+    GetPropos();
+  }, []);
+
+  return (
+    <>
+      <Table w="100%" variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Título:</Th>
+            <Th> Visualizar</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {notificacao.map((item, index) => (
+            <Tr key={index}>
+              <Td>{item.title}</Td>
+              <Td>
+                <Flex>
+                  <AprovarProposta
+                    id={item.id}
+                    description={item.description}
+                    title={item.title}
+                    userCreate={item.userCreate}
+                  />
+                </Flex>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </>
+  );
+}
