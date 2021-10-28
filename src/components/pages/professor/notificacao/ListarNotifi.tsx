53ae412d4f081f6e2d37d05982e11799f1a80f26
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2021-10-27 20:17:01 -0400

    style: centralize and justify list notifiction

--- src/components/pages/professor/notificacao/ListarNotifi.tsx
+++ src/components/pages/professor/notificacao/ListarNotifi.tsx
@@ -1,22 +1,10 @@
-import {
-  Flex,
-  Table,
-  Thead,
-  Tr,
-  Th,
-  Tbody,
-  Td,
-  Box,
-  Text,
-  HStack,
-} from "@chakra-ui/react";
+import { Flex, Table, Thead, Tr, Th, Tbody, Td } from "@chakra-ui/react";
 
 import { api } from "services/api";
 import { useEffect, useState } from "react";
 import useAuth from "hooks/auth";
 import EditarNotifi from "./EditarNotifi";
 import DeletNotifi from "./DeletNotifi";
-import { BoxShadown } from "components/shared/Box";
 
 type NotificacaoResponse = {
   id: string;
@@ -36,30 +24,20 @@ export function ListarNotifi() {
       });
 
       setNotifi(response.data);
-      ////console.log(response);
+      //console.log(response);
     }
     GetCursos();
   }, [DeletNotifi, EditarNotifi]);
 
   return (
     <>
-      {/* <Box w="100%">
-        {" "}
-        {notificacao.map((item, index) => (
-          <HStack border="1px" borderColor="gray.200" my={3}>
-            <Text noOfLines={1}>{item.description}</Text>
-
-            <EditarNotifi id={item.id} description={item.description} />
-            <DeletNotifi id={item.id} />
-          </HStack>
-        ))}
-      </Box> */}
-
       <Table w="100%" variant="unstyled" border="none">
         <Thead>
           <Tr>
             <Th>Nome:</Th>
-            <Th isNumeric>Editar/Excluir</Th>
+            <Th>
+              <Flex justifyContent="flex-end"> Editar/Excluir</Flex>
+            </Th>
           </Tr>
         </Thead>
         <Tbody>
@@ -67,12 +45,12 @@ export function ListarNotifi() {
             <Tr
               key={index}
               _hover={{
-                bg: "gray.100",
+                bg: "gray.200",
               }}
             >
               <Td noOfLines={1}>{item.description}</Td>
               <Td>
-                <Flex justifyContent='flex-end'>
+                <Flex justifyContent="flex-end">
                   <EditarNotifi id={item.id} description={item.description} />
                   <DeletNotifi id={item.id} />
                 </Flex>
