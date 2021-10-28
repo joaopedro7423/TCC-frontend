1d70f1706084802bd99e7ef269f8c41a4579c0f6
Joao Pedro Moreira Palma <joaopedro7423@hotmail.com>
2021-10-27 21:47:51 -0400

    fix: rest and children Box shered

--- src/components/shared/Box.tsx
+++ src/components/shared/Box.tsx
@@ -1,7 +1,11 @@
-import { Box } from "@chakra-ui/react";
 import React, { ReactNode } from "react";
+import { Box, BoxProps } from "@chakra-ui/react";
 
-export function BoxShadown({ children, ...rest }: { children: ReactNode }) {
+interface ContentProps extends BoxProps {
+  children: ReactNode;
+}
+
+export function BoxShadown({ children, ...rest }: ContentProps) {
   return (
     <>
       <Box
