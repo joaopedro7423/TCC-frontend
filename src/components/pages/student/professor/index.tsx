7bfce9310c22bd709e1231b311815275eb0951d1
Joao Pedro <joaopedro7423@hotmail.com>
2021-10-28 14:02:33 -0300

    refactor: remake style in list professors proposals

--- src/components/pages/student/professor/index.tsx
+++ src/components/pages/student/professor/index.tsx
@@ -1,14 +1,17 @@
 import { Heading, Text } from "@chakra-ui/layout";
 import { Flex } from "@chakra-ui/react";
+import { BoxShadown } from "components/shared/Box";
 import { ListarPropostaProfessor } from "./ListarPropostaProfessor";
 
 export function PropostasProfessorPage() {
   return (
     <>
-      <Heading size="lg"> Listagem de Propostas dos Professores:</Heading>
-      <Flex p={3}>
-        <ListarPropostaProfessor />
-      </Flex>
+      <BoxShadown>
+        <Heading ml={7} size="lg"> Listagem de Propostas dos Professores:</Heading>
+        <Flex p={3}>
+          <ListarPropostaProfessor />
+        </Flex>
+      </BoxShadown>
     </>
   );
 }
